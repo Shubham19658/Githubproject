6f868eac3e11c381399390619af4d39d3de9e29e
Shubham <Shubham@DESKTOP-4800R0V>
2022-06-15 20:00:53 +0530

    bvy em

---
+++ Github/Theory/Demo2.java
@@ -0,0 +1,21 @@
+
+public class Demo2 {
+
+	
+	
+	public void m1()
+	{
+		System.out.println("method m1");
+	}
+	
+	public void m2()
+	{
+		System.out.println("method m2");
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("main method");
+	}
+}
